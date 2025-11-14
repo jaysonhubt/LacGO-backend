9308013fa896db53ba6797c029f535297edcf3b3
SonTH-LLine <tran-hong-son@lline-group.vn>
2025-11-14 18:27:52 +0700

    Migration: user

--- code/database/migrations/0001_01_01_000000_create_users_table.php
+++ code/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone')->unique()->nullable();
+            $table->date('dob')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedTinyInteger('type');
+            $table->boolean('status')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
